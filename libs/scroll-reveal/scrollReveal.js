5a0e95627ddea9749d334974b055f0d6422c9c51
fafadevhub <fabriciopereiraleal2021@gmail.com>
2025-03-01 16:22:27 -0300

    Incluindo Lib Scroll Reveal

---
+++ libs/scroll-reveal/scrollReveal.js
@@ -0,0 +1,18 @@
+ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 650,
+    reset: true
+}).reveal(
+`#intro,
+#intro .content,
+#intro .content .info,
+#intro .content .tag,
+#intro .content .techs,
+#intro .content .techs .tech,
+#projects .content,
+#projects .content header,
+#intro .content .projects,
+#intro .content .projects .proj
+`
+)
